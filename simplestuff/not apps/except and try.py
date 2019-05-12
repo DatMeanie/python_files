4bc7cc662bb8514ce9e84d664b64af1aa26b573b
Albin Fyhr <31509099+DatMeanie@users.noreply.github.com>
2019-05-12 12:30:59 +0200

    Update except and try.py

--- simplestuff/not apps/except and try.py
+++ simplestuff/not apps/except and try.py
@@ -2,7 +2,7 @@ def spam(divideBy):
     try:
         return 42 / divideBy
     except ZeroDivisionError:
-        print('division by zero will implode the world, sorry nigga')
+        print('division by zero will implode the world, sorry')
 
 print(spam(2))
 print(spam(69))
